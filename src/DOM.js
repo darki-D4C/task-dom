a79e35cd683c51bed7dc55e497f686fb95ba776b
darki-D4C <den.pashmentov75@yandex.ru>
2022-06-05 12:05:32 +0300

    task is done

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,11 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+  for (let i = 0; i < count; i++) {
+    let temp = document.createElement(tag);
+    temp.innerHTML = content;
+    document.body.appendChild(temp);
+  }
 }
 
 /*
@@ -15,6 +20,31 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+  function buildTree(level, count) {
+    let root,
+        i = count - 1;
+    if (level > 0) {
+        if (document.getElementsByClassName('item_' + i).length == 0) {
+            root = document.createElement('div');
+            document.body.appendChild(root);
+            root.setAttribute('class', 'item_' + count);
+            buildTree(level - 1, count + 1);
+        } else {
+            for (root of document.getElementsByClassName('item_' + i)) {
+                for (let i = 0; i < childrenCount; i++) {
+                    let child = document.createElement('div');
+                    child.setAttribute('class', 'item_' + count);
+                    root.appendChild(child);
+                }
+            }
+            buildTree(level - 1, count + 1);
+            return root;
+        }
+    }
+    return root;
+  }
+  
+  return buildTree(level, 1);
 }
 
 /*
@@ -26,4 +56,22 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+  let tree = generateTree(2, 3);
+    let root = document.getElementsByClassName('item_1')[0];
+    for (let child of document.getElementsByClassName('item_2')) {
+        if (child.tagName != 'SECTION') {
+            let newNode = document.createElement('SECTION');
+            newNode.setAttribute('class', 'item_2');
+            for (let i = 0; i < 2; i++) {
+                let newChild = child.firstChild;
+                newNode.appendChild(newChild);
+            }
+            root.appendChild(newNode);
+        }
+    }
+    for (let i = 0; i < 2; i++) {
+        let deleteChild = document.getElementsByClassName('item_2')[0];
+        root.removeChild(deleteChild);
+    }
+    return tree;
 }
